6978e0c750015f17d90e906d6b49970049208107
minamakary <mina.makary@live.com>
2023-01-16 20:42:41 +0200

    modified the newScanner script to accept instructions and move the robot between obstacles

--- scripts/newScanner.py
+++ scripts/newScanner.py
@@ -6,13 +6,18 @@ def callback(msg):
 
 	print (msg.ranges [360])
 	move.linear.x = 0.1
-	if msg.ranges [360] < 1:
+	if msg.ranges [360] < 0.5:
 		move.linear.x = 0
 		pub.publish(move) 
+		
+	if msg.ranges [360] > 1:
+		move.linear.x = 0.1
+		pub.publish(move) 
+	
 rospy. init_node('check_obstacle')
 
 sub = rospy.Subscriber('/scan', LaserScan, callback)
-pub = rospy.Publisher('/rover/base_controller/cmd_vel', Twist)
+pub = rospy.Publisher('/rover/base_controller/cmd_vel', Twist, queue_size=10)
 
 move = Twist()
 
